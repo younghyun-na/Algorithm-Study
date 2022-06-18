74f3173a6a6002149a5dab32643d6bad8bd5d10e
younghyun-na <younghyunna@naver.com>
2022-06-18 19:52:02 +0900

    attempt: BOJ 4673

---
+++ BruteForce/4673.py
@@ -0,0 +1,15 @@
+not_selfnum = set()
+
+def d(n):
+    for i in str(n):
+        n += int(i)
+    return n
+
+for i in range(1, 10001):
+    not_selfnum.add(d(i))
+
+selfnum = set(range(1,10001)) - not_selfnum
+
+for num in sorted(selfnum):
+    print(num)
+
