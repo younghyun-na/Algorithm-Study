d045b4c4503933326d09c9bdfa2d7aa904630e98
younghyun-na <younghyunna@naver.com>
2022-12-01 14:35:10 +0900

    ✨ solved: Programmers 기능개발

---
+++ Stack, Queue/기능개발.py
@@ -0,0 +1,28 @@
+import math
+
+def solution(progresses, speeds):
+    stack = []
+    days = []
+    answer = [0] * len(progresses)
+    i = 0
+
+    for j in range(len(progresses)):
+        # days.append(math.ceil((100-progresses[j])/speeds[j]))
+        days.append(-(-100+progresses[j]//speeds[j]))
+        
+
+    for day in days:
+
+        if stack:
+
+            if day > stack[-1]:
+                stack.pop()
+                stack.append(day)
+                i += 1
+
+        else:
+            stack.append(day)
+            
+        answer[i] += 1
+        
+    return answer[0:i+1]
