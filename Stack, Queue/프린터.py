84d49369e9b4a9060e1b3136b2b0a827624923c2
younghyun-na <younghyunna@naver.com>
2022-12-12 13:30:00 +0900

    ✨ solved: Programmers 프린터

--- Stack, Queue/프린터.py
+++ Stack, Queue/프린터.py
@@ -8,21 +8,15 @@ def solution(priorities, location):
     while queue:
         
         if queue[0][0] == max(queue)[0]:
-            # temp = queue[0]
-            # queue.popleft()
             temp = queue.popleft()
             answer.append(temp)
 
         else:
-            # temp = queue[0]
-            # queue.popleft()
             temp = queue.popleft()
             queue.append(temp)
 
 
-    for i in range(len(answer)):
-        if answer[i][1] == location:
-            return i+1
+    for index in range(len(answer)):
+        if answer[index][1] == location:
+            return index+1
         
-print(solution([2, 1, 3, 2], 2))
-# print(solution([1, 1, 9, 1, 1, 1], 0))
