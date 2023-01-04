91c8b2b8964cf26adf24d18b5b45dc966ffda880
younghyun-na <younghyunna@naver.com>
2023-01-04 12:57:07 +0900

    🚧 attempt: BOJ 2493

---
+++ Stack, Queue/2493.py
@@ -0,0 +1,26 @@
+N = int(input())
+heights = list(map(int,input().split()))
+
+stack = []
+answer = []
+
+for i in range(N):   
+
+    while stack:
+
+        if stack[-1][1] > heights[i]:   
+            answer.append(stack[-1][0] + 1)
+            stack.append([i, heights[i]])
+            break
+
+        else:         
+            stack.pop()
+                
+    if not stack:
+        answer.append(0)
+        
+    stack.append([i, heights[i]])
+
+print(" ".join(map(str, answer)))
+
+     
