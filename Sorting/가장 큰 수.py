ba35c5ac71f632dabb0674bb443efd0984bdffc0
younghyun-na <younghyunna@naver.com>
2023-02-11 15:56:47 +0900

    🚧 attempt: Programmers 가장 큰 수

---
+++ Sorting/가장 큰 수.py
@@ -0,0 +1,7 @@
+def solution(numbers):
+    answer = ''
+    
+    numbers_str = [str(num) for num in numbers]
+    numbers_str.sort(key = lambda num:num*3, reverse = True)
+
+    return str(int(''.join(numbers_str)))
