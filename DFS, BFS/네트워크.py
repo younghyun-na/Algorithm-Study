6e9012f5e4490e7bea57887b0cd2da150f6dc165
younghyun-na <younghyunna@naver.com>
2023-02-26 13:28:58 +0900

    🚧 attempt: Programmers  네트워크

---
+++ DFS, BFS/네트워크.py
@@ -0,0 +1,18 @@
+def dfs(n, computers, start, visited):
+    visited[start] = True
+    for i in range(n):
+        if computers[start][i] == 1 and visited[i] == False:
+            dfs(n, computers, i, visited) 
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    for i in range(n):
+        if visited[i] == False:
+          dfs(n, computers, i, visited)
+          answer += 1
+    return answer
+
+
+
+print(solution(3,	[[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
