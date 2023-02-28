d0ad759035d5891f38e97f6202d97ce3b60a507e
younghyun-na <younghyunna@naver.com>
2023-02-28 13:21:39 +0900

    🚧 attempt: Programmers 게임 맵 최단거리

---
+++ DFS, BFS/게임 맵 최단거리.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+def solution(maps):
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    n = len(maps)
+    m = len(maps[0])
+
+    def bfs(x, y):
+     
+        queue = deque()
+        queue.append((x, y))
+        
+        while queue:
+            x, y = queue.popleft()
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+
+                if maps[nx][ny] == 0:
+                    continue
+
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    queue.append((nx, ny))
+            
+        return maps[n-1][m-1]
+    
+    answer = bfs(0,0)
+
+    if answer == 1: 
+        return -1
+    else: 
+        return answer
