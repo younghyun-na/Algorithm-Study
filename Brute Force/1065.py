3a1c86f7ed5fcb4f477f0b59ef4df931dcda6c10
younghyun-na <younghyunna@naver.com>
2023-04-02 18:23:00 +0900

    ✨ solved: BOJ 1065

--- Brute Force/1065.py
+++ Brute Force/1065.py
@@ -1,9 +1,7 @@
 N = int(input())
 hansu = []
 
-
 def search(n):
-
     arr = []
 
     for i in str(n):
@@ -13,10 +11,8 @@ def search(n):
         hansu.append(1)
     
     for i in range(1, len(arr), 1):   
-        
-        if (arr[i] - arr[i-1] == arr[1] - arr[0]):
-           
-            if (i == len(arr)-1):  
+        if arr[i] - arr[i-1] == arr[1] - arr[0]:
+            if i == len(arr)-1:
                 hansu.append(1)
 
 for n in range(1, N + 1):
