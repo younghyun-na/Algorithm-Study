403799bf0aaf62585b537e2245ca2627380a9f26
younghyun-na <younghyunna@naver.com>
2023-04-09 12:25:00 +0900

    ✨ solved: BOJ 2231

--- Brute Force/2231.py
+++ Brute Force/2231.py
@@ -1,19 +1,13 @@
 N = int(input())
-data = dict()
-x = N
+result = 0
 
 for i in range(N):
-    nums = list(map(int, str(i)))
-    bunhae = i + sum(nums)
-    data[i] = bunhae
+    arr = list(map(int, str(i)))
+    bunhae = i + sum(arr)
+    
+    if (bunhae == N):
+        result = i
+        break
 
-value_list = data.values()
+print(result)
 
-if N in value_list:
-    for i in range(N):
-      if (data[i] == N):
-        x = min(x, i)
-else:
-    x = 0
-
-print(x)
