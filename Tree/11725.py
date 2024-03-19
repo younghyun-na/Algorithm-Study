43466feb147289a08ed11982789a0e58ce7b9632
younghyun-na <younghyunna@naver.com>
2024-03-19 14:05:07 +0900

    ✨ solved: Baekjoon 11725

--- Tree/11725.py
+++ Tree/11725.py
@@ -1,18 +1,8 @@
-# 1:03 ~ 
-# 루트 없는 트리가 주어진다. 이때, 트리의 루트를 1이라고 정했을 때, 각 노드의 부모를 구하는 프로그램
-
-# 1: 4 6
-# 2: 4
-# 3: 5 6 
-# 4: 1 2 7
-# 5: 3
-# 6: 1 3
-# 7: 4
-
-# 4 -> 2 7
-# 6 -> 3
+# 트리의 루트를 1이라고 정했을 때, 각 노드의 부모를 구하는 프로그램
 
 from collections import defaultdict
+import sys
+sys.setrecursionlimit(100000)
 
 n = int(input())
 graph = defaultdict(list)
@@ -24,12 +14,6 @@ for _ in range(n-1):
   graph[a].append(b)
   graph[b].append(a)
 
-# for node in graph[1]:
-#   answer[node] = 1
-
-# 시작 노드: 1
-# 방문 조건: graph[root], 이미 들린 노드 제외
-  
 def dfs(x):
   for node in graph[x]:
     if answer[node] == 0:
